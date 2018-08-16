e95fe23c57b007c9afe5754e1c29be32b4337106
manauwar <manauwar.alam@outlook.com>
2018-08-16 15:46:46 +0530

    Project converted to Maven

---
+++ src/test/java/com/manauwar/unitTest/MyTests.java
@@ -0,0 +1,19 @@
+package com.manauwar.unitTest;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyTests {
+	
+	
+	@Test
+	public void searchTests()
+	{
+		
+		assertEquals
+		
+		
+		
+	}
+
+}
