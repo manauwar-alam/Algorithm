58071e54b3c190e5beb4ff2ccf064ffe24cc74d8
manauwar <manauwar.alam@outlook.com>
2018-08-26 23:53:15 +0530

    Lambdas Examples added

---
+++ src/main/java/com/manauwar/java8/lambda/unit1/Greeting.java
@@ -0,0 +1,10 @@
+package com.manauwar.java8.lambda.unit1;
+
+@FunctionalInterface
+public interface Greeting {
+	public void perform();
+	
+	
+
+
+}
---
+++ src/main/java/com/manauwar/java8/lambda/unit1/HelloWorldGreeting.java
@@ -0,0 +1,11 @@
+package com.manauwar.java8.lambda.unit1;
+
+public class HelloWorldGreeting implements Greeting {
+
+	@Override
+	public void perform() {
+		System.out.print("Hello world!");
+
+	}
+
+}
